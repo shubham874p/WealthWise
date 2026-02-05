42e73fcc21d980a4ca90fdcb879664c78914da45
shubham874p <140966043+shubham874p@users.noreply.github.com>
2026-02-06 04:50:29 +0530

    Add files via upload

---
+++ WealthWiseApplication.java
@@ -0,0 +1,13 @@
+package com.wealth_wise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthWiseApplication.class, args);
+	}
+
+}
