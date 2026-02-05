0e76de0fa9a7bb4586b9335a0ae0f3fd71bd96cc
shubham874p <140966043+shubham874p@users.noreply.github.com>
2026-02-06 05:07:33 +0530

    Add DashboardController for dashboard data retrieval

---
+++ Controller/DashBoardController.java
@@ -0,0 +1,23 @@
+package com.wealth_wise.controller;
+
+
+import com.wealth_wise.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
+public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getDashboardData() {
+        Map<String, Object> dashboardData = dashboardService.getDashboardData();
+        return ResponseEntity.ok(dashboardData);
+    }
+}
