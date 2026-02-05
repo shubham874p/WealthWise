ae3c7eb9de202fe95e277168d24068633c4c1b9a
shubham874p <140966043+shubham874p@users.noreply.github.com>
2026-02-06 05:06:46 +0530

    Implement CategoryController with CRUD operations

---
+++ Controller/CategoryController.java
@@ -0,0 +1,44 @@
+package com.wealth_wise.controller;
+
+
+import com.wealth_wise.DTO.CategoryDTO;
+import com.wealth_wise.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/save")
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> categories = categoryService.getCategoriesForCurrentUser();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<CategoryDTO>> getCategoriesByTypeForCurrentUser(@PathVariable String type) {
+        List<CategoryDTO> list = categoryService.getCategoriesByTypeForCurrentUser(type);
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId,
+                                                      @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryId, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
+    }
+}
